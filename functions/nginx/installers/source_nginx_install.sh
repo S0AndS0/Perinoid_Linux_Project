fe3e025640c67cfdccf8a9bfa0808f595e086d06
S0AndS0 <strangerthanbland@gmail.com>
2016-01-18 13:13:17 -0800

    Corrected typo and added sudo-code for when ModSecurity is fully incorporated.

--- functions/nginx/installers/source_nginx_install.sh
+++ functions/nginx/installers/source_nginx_install.sh
@@ -7,13 +7,18 @@ Source_nginx_install(){
 
 	make clean;\
 	 ./configure --with-http_ssl_module --with-http_spdy_module --with-http_gzip_static_module --with-file-aio\
-	 --without-http_autoindex_module --without-http_browser_module --without-http_fastcgi_module --eithout-http_geo_module\
+	 --without-http_autoindex_module --without-http_browser_module --without-http_fastcgi_module --without-http_geo_module\
 	 --without-http_empty_gif_module --without-http_map_module --without-http_proxy_module\
 	 --without-http_memcached_module --without-http_ssl_module --without-http_userid_module\
 	 --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module\
 	 --without-http_split_clients_modual --without-http_uwsgi_module --without-http_scgi_module\
 	 --wthout-http_limit_conn_module --without-http_referer_module --without-http-cache\
-	 --without-http_upstream_ip_hash_module &&\
+	 --without-http_upstream_ip_hash_module\
+#if [  -f "../mod_security/nginx/modsecurity" ]; then
+#	 --add-module=../mod_security/nginx/modsecurity &&\
+#else
+	 &&\
+#fi
 	 make &&\
 	 make install
 }
