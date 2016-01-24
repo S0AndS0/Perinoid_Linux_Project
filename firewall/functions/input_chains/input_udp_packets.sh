3819b388803e4375c344c205ec16461a4acf205d
S0AndS0 <strangerthanbland@gmail.com>
2016-01-23 22:05:22 -0800

    Added `for` loop for handling multiple DNS ports in and out.

--- firewall/functions/input_chains/input_udp_packets.sh
+++ firewall/functions/input_chains/input_udp_packets.sh
@@ -3,8 +3,10 @@ Input_udp_packets(){
 	iptables -A in_udp -p UDP --dport 137 -j DROP -m comment --comment "Drop packets from chatty Microsoft devices" || exit 1
 	iptables -A in_udp -p UDP --dport 138 -j DROP -m comment --comment "Drop packets from chatty Microsoft devices" || exit 1
 	for _dns_addr in ${_dns_addresses:-$(cat /etc/resolv.conf | grep -vE '#' | awk '/nameserver/{print $2}')}; do
-		iptables -A in_udp -p UDP -i ${_lo_iface} -s ${_lo_ipv4} -d ${_dns_addr} --dport ${_dns_port} -m state --state ESTABLISHED,RELATED -j ACCEPT -m comment --comment "Allow only established and related connections to loopback for DNS requests" || exit 1
-		iptables -A in_udp -p UDP -i ${_server_iface} -s ${_server_ipv4} -d ${_dns_addr} --dport ${_dns_port} -m state --state ESTABLISHED,RELATED -j ACCEPT -m comment --comment "Allow only established and related connections to server interface for DNS requests" || exit 1
+		for _dns_port in ${_dns_ports//,/ }; do
+			iptables -A in_udp -p UDP -i ${_lo_iface} -s ${_lo_ipv4} -d ${_dns_addr} --dport ${_dns_port} -m state --state ESTABLISHED,RELATED -j ACCEPT -m comment --comment "Allow only established and related connections to loopback for DNS requests" || exit 1
+			iptables -A in_udp -p UDP -i ${_server_iface} -s ${_server_ipv4} -d ${_dns_addr} --dport ${_dns_port} -m state --state ESTABLISHED,RELATED -j ACCEPT -m comment --comment "Allow only established and related connections to server interface for DNS requests" || exit 1
+		done
 	done
 	## Uncomment to allow others to make NTP requests of your server
 	#iptables -A in_udp -p UDP -i ${_server_iface} --dport 123 -m state --state NEW -j ACCEPT -m comment --comment "Allow new NTP requests into server" || exit 1
