26464bfe1e0db8551d17ed8831be7d28c6d56bfd
S0AndS0 <strangerthanbland@gmail.com>
2016-01-24 23:21:52 -0800

    Corrected missing calls to help documentation if failed to be read arguments.

--- functions/nginx/installers/aptget_nginx_install.sh
+++ functions/nginx/installers/aptget_nginx_install.sh
@@ -1,6 +1,6 @@
 Aptget_nginx_install(){
-	Arg_checker "${@:---help='' --exit=''}" -ep='Aptget_nginx_install'
-	echo "## Notice [Aptget_nginx_install}] sending apps [${_application_list}] to [Install_Apt] function..."
+	Arg_checker "${@:---help='Aptget_nginx_install' --exit='# [Aptget_nginx_install] # Failed to be read arguments'}" -ep='Aptget_nginx_install'
+	echo "## Notice [Aptget_nginx_install] sending apps [${_application_list}] to [Install_Apt] function..."
 	Install_Apt "${_application_list}"
 }
 ### Aptget_nginx_install_help aptget_nginx_install_help aptget_nginx_install.sh
