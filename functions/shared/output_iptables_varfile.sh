5ccc61e607b3277b1addca0be735c899f0e413bf
S0AndS0 <strangerthanbland@gmail.com>
2016-01-25 01:02:35 -0800

    Completed internal to-do list and added to help documentation. Future pushes should include assigning ports and addresses to variables and writing it out to custom firewall configuration file.

--- functions/shared/output_iptables_varfile.sh
+++ functions/shared/output_iptables_varfile.sh
@@ -1,6 +1,16 @@
 Output_iptables_varfile(){
 
-## TO-DO : Add tcp ports to ${_tcp_ports} variable based on application being configured
+## TO-DO : Add the following variable assignments based on installation/configuration
+## options chosen.
+#	_tcp_ports=""
+#	_make_bridge_forwarding=""
+#	_bridge_internal_iface=""
+#	_bridge_port_pares=""
+#	_enable_forwarding=""
+#	_dns_port=""
+#	_ssh_port=""
+#	_http_port=""
+#	_ssl_port=""
 
 	Arg_checker "${@:---help='Output_iptables_varfile' --exit='# [Output_iptables_varfile] # Failed to be read arguments'}" -ep='Output_iptables_varfile'
 	_vars_out_file="${_script_dir}/iptables/vars/user_configs.sh"
@@ -91,8 +101,49 @@ Output_iptables_varfile(){
 	
 	
 
+	# _incoming_tcp_update_seconds="60"
+	# _incoming_tcp_hitcount="8"
+	# _incoming_tcp_update_seconds="300"
+	# _incoming_tcp_burst_minute="25"
+	# _incoming_tcp_burst_limit="100"
+	# _outgoing_tcp_update_seconds="60"
+	# _outgoing_tcp_hitcount="8"
+	# _outgoing_tcp_update_seconds="300"
+	# _outgoing_tcp_burst_minute="25"
+	# _outgoing_tcp_burst_limit="100"
+
 }
 ### Output_iptables_varfile_help output_iptables_varfile_help output_iptables_varfile.sh
 #	File : ${_script_dir}/functions/shared/output_iptables_varfile.sh
-#	
+#	Argument		Variable					Usage
+#	-B9ipv4			_bind9_ipv4					Sets IP address that Bind9 services will bind to
+#	-B9P			_bind9_port					Sets port that Bind9 services will bind to
+#	-FBI			_firejail_bridge_interface	Sets interface name that Firejail will bind to
+#	-FBF			_firejail_bridge_forward	Sets wheather or not to forward connections into firejail'ed network
+#	-FHI			_firejail_host_interface	Sets hosts interface name that packets will be forwarded through
+#	-FBipv4			_firejail_bridge_ipv4		Sets
+#	-FNipv4			_firejail_nat_ipv4			Sets 
+#	-NHP			_nginx_http_port			Sets port that Nginx will bind to for http traffic
+#	-NSP			_nginx_ssl_port				Sets port that Nginx will bind to for ssl traffic
+#					_tor_or_port				Sets
+#					_tor_dns_port				Sets
+#					_tor_dir_port				Sets 
+#					_tor_web_port				Sets
+#					_bridge_ipv4_to_add			Sets
+#					_bridge_ipv6_to_add			Sets
+#					_bridge_port_to_add			Sets
+#					_openssh_port				Sets
+#					_tor_ssh_port				Sets
+#					_tor_socks_bind_address		Sets
+#					_tor_socks_listen_address	Sets
+#					_tor_relay_bandwidth_rate	Sets
+#					_tor_relay_bandwidth_burst	Sets
+#					_external_ipv4				Sets
+#					_external_ipv6				Sets
+#					_enable_ipv6				Sets
+#					_admin_ipv4					Sets
+#					_admin_ipv6					Sets
+#					_nat_ipv4					Sets
+#					_nat_ipv6					Sets
 ####
+
