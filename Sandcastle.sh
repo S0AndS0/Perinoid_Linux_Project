72334ca7bfbb20d277d92333f3103863537eca22
S0AndS0 <strangerthanbland@gmail.com>
2016-01-24 23:24:06 -0800

    Rewrote to allow for `ALL` to be an option for any of the available coded configurations.

--- Sandcastle.sh
+++ Sandcastle.sh
@@ -25,55 +25,55 @@ Sandcastle(){
 		echo "#	[${_script_name}] parsing what applications to install and or configure"
 		for _apps in ${_application_list//,/ }; do
 			case ${_apps} in
-			nginx)
+			nginx|ALL)
 				source "${_script_dir}/functions/nginx/source_nginx_functions.sh" || Arg_checker --help="${_script_name},source_nginx_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/nginx/source_nginx_functions.sh]"
 				Source_nginx_functions "${_script_dir}" || Arg_checker --help="${_script_name},Source_nginx_functions" --exit="#${_now}# Error reading functions into [${_script_name}] for [Source_nginx_functions]"
 				_application_list="${_application_list:-nginx},nginx-common,nginx-full,libgd3"
 				Order_nginx_install "${_script_args}"
 				
 			;;
-			tor)
+			tor|ALL)
 				source "${_script_dir}/functions/tor/source_tor_functions.sh" || Arg_checker --help="${_script_name},Source_tor_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/tor/source_tor_functions.sh]"
 				Source_tor_functions "${_script_dir}" || Arg_checker --help="${_script_name},Source_tor_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] for [Source_tor_functions]"
 				_application_list="${_application_list:-tor},tor-arm,tor-geoipdb,deb.torproject.org-keyring"
 				Order_tor_install "${_script_args}"
 			;;
-			privoxy)
+			privoxy|ALL)
 				source "${_script_dir}/functions/privoxy/source_privoxy_functions.sh" || Arg_checker --help="${_script_name},Source_privoxy_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/privoxy/source_privoxy_functions.sh]"
 				Source_privoxy_functions "${_script_dir}" || Arg_checker --help="${_script_name}," --exit="#${_now}# Error pulling functions into [${_script_name}] for [Source_privoxy_functions]"
 				_application_list="${_application_list:-privoxy}"
 				Order_privoxy_install "${_script_args}"
 				
 			;;
-			polipo)
+			polipo|ALL)
 				source "${_script_dir}/functions/polipo/source_polipo_functions.sh" || Arg_checker --help="${_script_name},Source_polipo_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/polipo/source_polipo_functions.sh]"
 				Source_polipo_functions "${_script_dir}" || Arg_checker --help="${_script_name},Source_polipo_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] for [Source_polipo_functions]"
 				_application_list="${_application_list:-polipo}"
 				Order_polipo_install "${_script_args}"
 				
 			;;
-			squid|squid3)
+			squid|squid3|ALL)
 				source "${_script_dir}/functions/squid/source_squid_functions.sh" || Arg_checker --help="${_script_name},Source_squid_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/squid/source_squid_functions.sh]"
 				Source_squid_functions "${_script_dir}" || Arg_checker --help="${_script_name},Source_squid_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] for [Source_squid_functions]"
 				_application_list="${_application_list:-squid3}"
 				Order_squid_install "${_script_args}"
 				
 			;;
-			bind|bind9)
+			bind|bind9|ALL)
 				source "${_script_dir}/functions/bind/source_bind_functions.sh" || Arg_checker --help="${_script_name},Source_bind_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/bind/source_bind_functions.sh]"
 				Source_bind_functions "${_script_dir}" || Arg_checker --help="${_script_name},Source_bind_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] for [Source_bind_functions]"
 				_application_list="${_application_list:-bind9},dnsutils"
 				Order_bind_install "${_script_args}"
 				
 			;;
-			firejail)
+			firejail|ALL)
 				source "${_script_dir}/functions/firejail/source_firejail_functions.sh" || Arg_checker --help="${_script_name},Source_firejail_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/firejail/source_firejail_functions.sh]"
 				Source_firejail_functions "${_script_dir}" || Arg_checker --help="${_script_name},Source_firejail_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] for [Source_firejail_functions]"
 				_application_list="${_application_list:-firejail},bridge-utilss"
 				Order_firejail_install "${_script_args},debootstrap,bridge-utils"
 				
 			;;
-			firetools)
+			firetools|ALL)
 				source "${_script_dir}/functions/firetools/source_firetools_functions.sh" || Arg_checker --help="${_script_name},Source_firetools_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/firetools/source_firetools_functions.sh]"
 				Source_firejail_functions "${_script_dir}" || Arg_checker --help="${_script_name},Source_firetools_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] for [Source_firetools_functions]"
 				_application_list="${_application_list:-firetools},gcc,make"
@@ -99,27 +99,6 @@ Sandcastle(){
 				Source_firejail_functions "${_script_dir}" || Arg_checker --help="${_script_name},Source_firejail_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] for [Source_firejail_functions]"
 				source "${_script_dir}/functions/firetools/source_firetools_functions.sh" || Arg_checker --help="${_script_name},Source_firetools_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/firetools/source_firetools_functions.sh]"
 				Source_firejail_functions "${_script_dir}" || Arg_checker --help="${_script_name},Source_firetools_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] for [Source_firetools_functions]"
-			;;
-			ALL)
-				echo "## Oh my... [${USER}] wants it all from [${_script_name}]?"
-				echo '#	away we go, hold on tight...'
-				source "${_script_dir}/functions/nginx/source_nginx_functions.sh" || Arg_checker --help="${_script_name},source_nginx_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/nginx/source_nginx_functions.sh]"
-				Source_nginx_functions "${_script_dir}" || Arg_checker --help="${_script_name},Source_nginx_functions" --exit="#${_now}# Error reading functions into [${_script_name}] for [Source_nginx_functions]"
-				source "${_script_dir}/functions/tor/source_tor_functions.sh" || Arg_checker --help="${_script_name},Source_tor_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/tor/source_tor_functions.sh]"
-				Source_tor_functions "${_script_dir}" || Arg_checker --help="${_script_name},Source_tor_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] for [Source_tor_functions]"
-				source "${_script_dir}/functions/privoxy/source_privoxy_functions.sh" || Arg_checker --help="${_script_name},Source_privoxy_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/privoxy/source_privoxy_functions.sh]"
-				Source_privoxy_functions "${_script_dir}" || Arg_checker --help="${_script_name}," --exit="#${_now}# Error pulling functions into [${_script_name}] for [Source_privoxy_functions]"
-				source "${_script_dir}/functions/polipo/source_polipo_functions.sh" || Arg_checker --help="${_script_name},Source_polipo_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] from [${_script_dir}/functions/polipo/source_polipo_functions.sh]"
-				Source_polipo_functions "${_script_dir}" || Arg_checker --help="${_script_name},Source_polipo_functions" --exit="#${_now}# Error pulling functions into [${_script_name}] for [Source_polipo_functions]"
-				_application_list="${_application_list:-nginx},nginx-common,nginx-full,libgd3"
-				Order_nginx_install "${_script_args}"
-				_application_list="${_application_list:-tor},tor-arm,tor-geoipdb,deb.torproject.org-keyring"
-				Order_tor_install "${_script_args}"
-				_application_list="${_application_list:-privoxy}"
-				Order_privoxy_install "${_script_args}"
-				_application_list="${_application_list:-polipo}"
-				Order_polipo_install "${_script_args}"
-
 			;;
 			*)
 				Arg_checker --help="${_script_name}" --exit="# [${_script_name}] # Failed to understand [${_apps}] in [${_application_list}]"
